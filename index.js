b5c8d82102769cdb84372d69571c1d2717141f65
sfDesat <75356901+sfDesat@users.noreply.github.com>
2025-06-06 17:36:24 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`Firebase script hosted on port ${PORT}`);
+});
